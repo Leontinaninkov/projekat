99d7292132ea1a55614c8d1a627188981db3b913
Leontinaninkov <73234717+Leontinaninkov@users.noreply.github.com>
2020-12-30 15:04:11 +0100

    dodat projekat

---
+++ knjige/knjigaIO.py
@@ -0,0 +1,12 @@
+import json
+
+
+datoteka = './datoteke/knjige.json'
+
+def sacuvaj_knjige(knjige):
+    with open(datoteka, "w") as f:
+        json.dump(knjige, f, indent=4)
+
+def ucitaj_knjige():
+    with open(datoteka) as f:
+        return json.load(f)
